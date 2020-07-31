ee2a65bac2c8019bdc084c6e1cfc0c9c110227bb
khcanniff <k_canniff@u.pacific.edu>
2020-07-31 16:31:35 -0700

    added in basic file pathing an text read in

---
+++ Climate AR App/Assets/scripts/UI/GetFilePath.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetFilePath : MonoBehaviour
+{
+    private string splitBy = "Climate AR App";
+    string buildPath;
+    string pathPreFix;
+    string[] files;
+    Texture2D[] textList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    /**
+     * pass in the folder name(s) you want to go to
+     * returns the full relative path name 
+     */
+    public string GetBuildPathFor(string folderName)
+    {
+        buildPath = Application.dataPath + "/../"; //need to triverse up a folder
+        //should have this in a try catch
+        //string finalPath = @"C:\Users\Keely\Pictures\altspace"; //change this to be under the build's folder
+        string finalPath = buildPath;
+
+        if (buildPath.Contains(splitBy))
+        {
+            string[] stringSeparators = new string[] { splitBy }; //make the string into an array
+
+            //int index = buildPath.IndexOf("MuirTouch");
+            //Debug.Log("Testing Pathing logic, index is "+ index);
+            string[] splitPath = buildPath.Split(stringSeparators, StringSplitOptions.None);
+            //Debug.LogError("splitPath size is " + splitPath.Length + " item at 0 is " + splitPath[0]);
+            //Debug.LogError("splitPath size is " + splitPath.Length + " item at 1 is " + splitPath[1]);
+            finalPath = splitPath[0] + "/" + splitBy + "/" + folderName;
+            //Debug.LogError("finalPath is " + finalPath);
+
+        }
+        else
+        {
+            //give msg
+            //exit application
+            Debug.LogError("path doesnt contain " + splitBy);
+        }
+        return finalPath;
+    }
+
+
+
+}
---
+++ Climate AR App/Assets/scripts/UI/ReadInText.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[RequireComponent(typeof(GetFilePath))]
+public class ReadInText : MonoBehaviour
+{
+    string folderName = "Data";
+    string[] files;
+    public string fileName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Climate AR App
+        ReadText(fileName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReadText(string NameOfTextFile)
+    {
+        string finalPath = this.GetComponent<GetFilePath>().GetBuildPathFor(folderName);
+        Debug.Log("finalPath " + finalPath);
+        files = System.IO.Directory.GetFiles(finalPath, "*.txt"); //get all of the text files
+        //Debug.Log("files size " + files.Length);
+        //Debug.Log("files[0] " + files[0]);
+
+        for(int i = 0; i < files.Length; i++)
+        {
+            if (files[i].Contains(NameOfTextFile))
+            {
+                Debug.Log("Found File");
+                Debug.Log("files[" + i + "] " + files[i]);
+            }
+        }
+
+        Debug.Log(files[0]);
+        StreamReader inp_stm = new StreamReader(files[0]);
+        while (!inp_stm.EndOfStream)
+        {
+            string inp_ln = inp_stm.ReadLine();
+            // Do Something with the input. 
+            Debug.Log(inp_ln);
+        }
+
+        inp_stm.Close();
+    }
+}
