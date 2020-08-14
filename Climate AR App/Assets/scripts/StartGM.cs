1c40e95a8eb3cb0a4d268acee46f75408c28112d
the_fashionable_coder <k_canniff@u.pacific.edu>
2020-08-14 09:27:56 -0700

    added welcome popup

---
+++ Climate AR App/Assets/scripts/StartGM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject welcomePopup;
+    void Start()
+    {
+        welcomePopup.SetActive(true);
+    }
+
+    public void CloseWelcomePopUp()
+    {
+        welcomePopup.SetActive(false);
+
+    }
+}
