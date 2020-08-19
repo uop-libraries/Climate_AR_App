379682394f5e17964a28720fffe4efbc5abea5f0
the_fashionable_coder <k_canniff@u.pacific.edu>
2020-08-18 18:28:44 -0700

    adjusted position of visuals

--- Climate AR App/Assets/scripts/StartGM.cs
+++ Climate AR App/Assets/scripts/StartGM.cs
@@ -6,9 +6,12 @@ public class StartGM : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject welcomePopup;
+    public GameObject bubbles;
     void Start()
     {
         welcomePopup.SetActive(true);
+        bubbles.SetActive(false);
+
     }
 
     public void CloseWelcomePopUp()
