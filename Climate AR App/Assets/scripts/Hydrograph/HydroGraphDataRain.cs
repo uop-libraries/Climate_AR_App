ddb7e6e8f8194a557f435c6362bdc855b113a3ac
the_fashionable_coder <k_canniff@u.pacific.edu>
2020-08-31 15:06:10 -0700

    have three graphs on the chart
    the charts work with delays and limits. I'm still working on getting the slope to change for the park land graph

--- Climate AR App/Assets/scripts/Hydrograph/HydroGraphDataRain.cs
+++ Climate AR App/Assets/scripts/Hydrograph/HydroGraphDataRain.cs
@@ -8,28 +8,28 @@ using Random = UnityEngine.Random;
 public class HydroGraphDataRain : MonoBehaviour
 {
     public GraphChart Graph;
-    float lastTime = 0f;
-    float lastX = 0f;
-    float waterLimit = 3f;
+    public float waterLimit = 3f;
+    public float waterAddAmount = .5f;
+    public int waterPeakFlatlineTime = 10;
+    float lastTime = 0f; 
     float waterLevelXValue = 0f;
-    float waterAddAmount =.5f;
     float waterLevelYValue = 0f;
     bool rainIsPeaked = false;
-    int waterPeakFlatlineTime = 10;
     // Start is called before the first frame update
     void Start()
     {
         if (Graph == null) // the ChartGraph info is obtained via the inspector
             return;
-        /////   Graph.DataSource.StartBatch(); // do not call StartBatch for realtime calls , it will only slow down performance.
-
-        Graph.DataSource.ClearCategory("Player 1"); // clear the "Player 1" category. this category is defined using the GraphChart inspector
+        Graph.DataSource.ClearCategory("Rain"); // clear the "Player 1" category. this category is defined using the GraphChart inspector
                                                     //Graph.DataSource.ClearCategory("Player 2"); // clear the "Player 2" category. this category is defined using the GraphChart inspector
-        Graph.DataSource.AddPointToCategoryRealtime("Player 1", waterLevelXValue, waterLevelYValue, 1f); // each time we call AddPointToCategory 
+        Graph.DataSource.AddPointToCategoryRealtime("Rain", waterLevelXValue, waterLevelYValue, 1f); // each time we call AddPointToCategory 
 
     }
 
     // Update is called once per frame
+    /*
+     * Handles the increase, flatlining and decreasing graph for the rain.
+     */
     void Update()
     {
         float time = Time.time;
@@ -41,13 +41,13 @@ public class HydroGraphDataRain : MonoBehaviour
 
             if (!rainIsPeaked) //increase graph
             {
-                float tempWaterLevelYValue = Random.Range(waterLevelYValue, (waterLevelYValue + waterAddAmount));
-                if (tempWaterLevelYValue <= waterLimit && tempWaterLevelYValue > 0f)
+                float tempWaterLevelYValue = Random.Range(waterLevelYValue, (waterLevelYValue + waterAddAmount)); //want some variation to the graph
+                if (tempWaterLevelYValue <= waterLimit && tempWaterLevelYValue > 0f) 
                 {
                     waterLevelYValue = tempWaterLevelYValue;
-                    Graph.DataSource.AddPointToCategoryRealtime("Player 1", waterLevelXValue, waterLevelYValue, 1f); // each time we call AddPointToCategory
+                    Graph.DataSource.AddPointToCategoryRealtime("Rain", waterLevelXValue, waterLevelYValue, 1f); // each time we call AddPointToCategory
                 }
-                if (Math.Abs(waterLevelYValue - waterLimit) <= .5f)
+                if (Math.Abs(waterLevelYValue - waterLimit) <= .5f) //once the limit is reached, start the flatlining
                 {
                     rainIsPeaked = true;
                 }
@@ -56,16 +56,16 @@ public class HydroGraphDataRain : MonoBehaviour
             else if (rainIsPeaked && waterPeakFlatlineTime >=0) //flatline
             {
                 waterPeakFlatlineTime--;
-                Graph.DataSource.AddPointToCategoryRealtime("Player 1", waterLevelXValue, waterLevelYValue, 1f); // each time we call AddPointToCategory 
+                Graph.DataSource.AddPointToCategoryRealtime("Rain", waterLevelXValue, waterLevelYValue, 1f); // add points to the graph
             }
             else if (waterLevelYValue - waterAddAmount >= 0f) //decrease graph
             {
                 Debug.Log("waterlevel is bigger thans 0");
-                float tempWaterLevelYValue = Random.Range(waterLevelYValue - waterAddAmount, waterLevelYValue);
+                float tempWaterLevelYValue = Random.Range(waterLevelYValue - waterAddAmount, waterLevelYValue); //want some variation to the graph
                 if (tempWaterLevelYValue > 0f)
                 {
                     waterLevelYValue = tempWaterLevelYValue;
-                    Graph.DataSource.AddPointToCategoryRealtime("Player 1", waterLevelXValue, waterLevelYValue, 1f); // each time we call AddPointToCategory 
+                    Graph.DataSource.AddPointToCategoryRealtime("Rain", waterLevelXValue, waterLevelYValue, 1f); // each time we call AddPointToCategory 
                 }
           
             }
@@ -75,5 +75,15 @@ public class HydroGraphDataRain : MonoBehaviour
     public bool GetRainPeakedBool()
     {
         return rainIsPeaked;
+    }
+
+    public float GetWaterLevelXValue()
+    {
+        return waterLevelXValue;
+    }
+
+    public float GetWaterLevelYValue()
+    {
+        return waterLevelYValue;
     }
 }
--- Climate AR App/Assets/scripts/Hydrograph/HydroGraphLands.cs
+++ Climate AR App/Assets/scripts/Hydrograph/HydroGraphLands.cs
@@ -3,78 +3,169 @@ using System.Collections.Generic;
 using UnityEngine;
 using ChartAndGraph;
 using System;
-
+using Random = UnityEngine.Random;
 public class HydroGraphLands : MonoBehaviour
 {
     public GraphChart Graph;
-    float cityXValue = 0f;
-    float cityYValue = 0f;
     float lastTime = 0f;
-    float cityLandIncrease = .01f;
-    bool landIsPeaked = false;
-    float maxCityLandLimit =4f;
-    int cityLineFlatlineTime = 5;
-    float tempCityLevelYValue = 0f;
+    public int parkLandDelayTimerLimit;
+    int parkTimer;
+    //------*city vars*------
+    public int cityLineFlatlineTime = 50;
+    public float cityLandIncrease = .01f;
+    public float maxCityLandLimit = 4f;
+    float cityXValue = .1f;
+    float cityYValue = .1f;
+    bool cityIsPeaked = false;
+    float tempCityLevelYValue = .5f;
+
+    //------*park vars*------
+    public int parkLineFlatlineTime = 2;
+    public float parkLandIncrease = .001f;
+    public float maxParkLandLimit = 3f;
+    public float flattenCurveLineForPark = 0f; 
+    float parkXValue = .1f;
+    float parkYValue = .1f;
+    bool parkIsPeaked = false;
+    float tempParkLevelYValue = .5f;
     // Start is called before the first frame update
+    //float oldParkYValue = 0f;
+    bool startParkGraph = false;
     void Start()
     {
         if (Graph == null) // the ChartGraph info is obtained via the inspector
             return;
-        Graph.DataSource.ClearCategory("Player 2"); // clear the "Player 1" category. this category is defined using the GraphChart inspector
-        Graph.DataSource.AddPointToCategoryRealtime("Player 2", cityXValue, cityYValue, 1f); // each time we call AddPointToCategory 
+        Graph.DataSource.ClearCategory("City Land"); // clear the "Player 1" category. this category is defined using the GraphChart inspector
+        Graph.DataSource.ClearCategory("Park Land"); // clear the "Player 1" category. this category is defined using the GraphChart inspector
+        Graph.DataSource.AddPointToCategoryRealtime("City Land", cityXValue, cityYValue, 1f); // each time we call AddPointToCategory 
+        Graph.DataSource.AddPointToCategoryRealtime("Park Land", cityXValue, cityYValue, 1f); // each time we call AddPointToCategory 
 
     }
 
     // Update is called once per frame
+    /*handles the increase, flatline and decreasing of the hyrograph for lands. 
+     * 
+     */
     void Update()
-    {
-        if (this.GetComponent<HydroGraphDataRain>().GetRainPeakedBool()) //rain is peaked
-        //if (true) //rain is peaked
+    {          
+        cityXValue = this.GetComponent<HydroGraphDataRain>().GetWaterLevelXValue();
+        parkXValue = this.GetComponent<HydroGraphDataRain>().GetWaterLevelXValue();
+        if (this.GetComponent<HydroGraphDataRain>().GetRainPeakedBool()) //rain is peaked, start the increase of the river flow increase
         {
+            cityLand(cityXValue);
+            parkLand(parkXValue);
+        }
+        else //the start flatlining of the river flow
+        {
+
+            Graph.DataSource.AddPointToCategoryRealtime("City Land", cityXValue, cityYValue, 1f); // each time we call AddPointToCategory
+            Graph.DataSource.AddPointToCategoryRealtime("Park Land", parkXValue, parkYValue, 1f); // each time we call AddPointToCategory
+        }
+
+        if (parkTimer  >= parkLandDelayTimerLimit)
+        {
+            startParkGraph = true;
+        }
+
+    }
+
+    void cityLand(float xValue)
+    {
+ 
             float time = Time.time;
             if (lastTime + 1f < time)
             {
-                cityXValue += cityLandIncrease;
-                Debug.Log("cityYValue is " + cityYValue);
-                if (!landIsPeaked) //increase graph
+            //cityXValue += cityLandIncrease;
+            parkXValue = xValue;
+            if (!cityIsPeaked) //increase graph
                 {
-                    Debug.Log("increase the graph");
-                    tempCityLevelYValue += cityLandIncrease;
+                    tempCityLevelYValue = Random.Range(cityYValue, (cityYValue + cityLandIncrease)); //want some variation to the graph
+                    //tempCityLevelYValue += cityLandIncrease;
                     if (tempCityLevelYValue <= maxCityLandLimit && tempCityLevelYValue > 0f)
                     {
-                        Debug.Log("set cityYvalue the graph");
                         cityYValue = tempCityLevelYValue;
-                        Debug.Log("cityYValue is " + cityYValue);
-                        Graph.DataSource.AddPointToCategoryRealtime("Player 2", cityXValue, cityYValue, 1f); // each time we call AddPointToCategory
-                    }
-                    else if(Math.Abs(cityYValue - maxCityLandLimit) <= .2f)
+                        Graph.DataSource.AddPointToCategoryRealtime("City Land", cityXValue, cityYValue, 1f); // each time we call AddPointToCategory
+                }
+                else if (Math.Abs(cityYValue - maxCityLandLimit) <= .2f)
                     {
                         tempCityLevelYValue = maxCityLandLimit;
-                        landIsPeaked = true;
+                        cityIsPeaked = true;
                     }
                 }
-                else if (landIsPeaked && cityLineFlatlineTime >= 0) //flatline
+                else if (cityIsPeaked && cityLineFlatlineTime >= 0) //flatline
                 {
-                    Debug.Log("flatline is reached");
                     cityLineFlatlineTime--;
-                    Graph.DataSource.AddPointToCategoryRealtime("Player 2", cityXValue, cityYValue, 1f); // each time we call AddPointToCategory 
+                    Graph.DataSource.AddPointToCategoryRealtime("City Land", cityXValue, cityYValue, 1f); // each time we call AddPointToCategory 
                 }
                 else if (cityYValue - cityLandIncrease >= 0f) //decrease graph
                 {
-                    Debug.Log("decrease cityYValue - cityLandIncrease >= 0f tempCityLevelYValue = " + tempCityLevelYValue);
-                    tempCityLevelYValue -= cityLandIncrease;
+                tempCityLevelYValue = Random.Range(cityYValue - cityLandIncrease, cityYValue); //want some variation to the graph
+                //tempCityLevelYValue -= cityLandIncrease;
                     if (tempCityLevelYValue >= 0f)
                     {
-                        Debug.Log("decrease");
                         cityYValue = tempCityLevelYValue;
-                        Graph.DataSource.AddPointToCategoryRealtime("Player 2", cityXValue, cityYValue, 1f); // each time we call AddPointToCategory 
+                        Graph.DataSource.AddPointToCategoryRealtime("City Land", cityXValue, cityYValue, 1f); // each time we call AddPointToCategory 
                     }
 
                 }
-            }
-            //start increase when rainPeaked is true
-            //increase the land line sharply until max is reached. 
-            //flatline a bit, then drop
+            parkTimer++;
         }
+
+
+    }
+
+    void parkLand(float xValue)  
+    {
+            float time = Time.time;
+            if (lastTime + 1f < time && startParkGraph)
+            {
+            //parkXValue += parkLandIncrease;
+            parkXValue = xValue;
+                if (!parkIsPeaked) //increase graph
+                {
+                //tempParkLevelYValue += parkLandIncrease;
+                tempParkLevelYValue = (Random.Range(parkYValue, (parkYValue + parkLandIncrease))) - flattenCurveLineForPark; //want some variation to the graph, need to flatten the curve since its park land
+                    if (tempParkLevelYValue <= maxParkLandLimit && tempParkLevelYValue > 0f && tempParkLevelYValue >= parkYValue)
+                    {
+                        parkYValue = tempParkLevelYValue;
+                        Graph.DataSource.AddPointToCategoryRealtime("Park Land", parkXValue, parkYValue, 1f); // each time we call AddPointToCategory
+                    }
+                    else if (Math.Abs(parkYValue - maxParkLandLimit) <= .2f)
+                    {
+                        tempParkLevelYValue = maxParkLandLimit;
+                        parkIsPeaked = true;
+                    }
+                    else
+                    {
+                        Graph.DataSource.AddPointToCategoryRealtime("Park Land", parkXValue, parkYValue, 1f); // each time we call AddPointToCategory 
+
+                    }
+                }
+                else if (parkIsPeaked && parkLineFlatlineTime >= 0) //flatline
+                {
+                    parkLineFlatlineTime--;
+                    Graph.DataSource.AddPointToCategoryRealtime("Park Land", parkXValue, parkYValue, 1f); // each time we call AddPointToCategory 
+                }
+                else if (parkYValue - parkLandIncrease >= 0f && parkIsPeaked) //decrease graph
+                {
+                tempParkLevelYValue = Random.Range(parkYValue - parkLandIncrease, parkYValue); //want some variation to the graph
+                //tempParkLevelYValue -= parkLandIncrease;
+                    if (tempParkLevelYValue >= 0f)
+                    {
+                        parkYValue = tempParkLevelYValue;
+                        Graph.DataSource.AddPointToCategoryRealtime("Park Land", parkXValue, parkYValue, 1f); // each time we call AddPointToCategory 
+                    }
+
+                }
+       
+
+            //oldParkYValue = parkYValue;
+             }
+             else if(!startParkGraph)
+             {
+                  Graph.DataSource.AddPointToCategoryRealtime("Park Land", parkXValue, parkYValue, 1f); // each time we call AddPointToCategory 
+
+               }
+
     }
 }
