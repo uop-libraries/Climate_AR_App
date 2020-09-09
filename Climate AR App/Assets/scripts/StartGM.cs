c5df9220532c6c0ff5282b748e47efb62a7f607b
the_fashionable_coder <k_canniff@u.pacific.edu>
2020-09-09 14:36:16 -0700

    fixed welcome screen

--- Climate AR App/Assets/scripts/StartGM.cs
+++ Climate AR App/Assets/scripts/StartGM.cs
@@ -7,16 +7,19 @@ public class StartGM : MonoBehaviour
     // Start is called before the first frame update
     public GameObject welcomePopup;
     public GameObject bubbles;
+    public GameObject POIs;
     void Start()
     {
         welcomePopup.SetActive(true);
         bubbles.SetActive(false);
+        POIs.SetActive(false);
 
     }
 
     public void CloseWelcomePopUp()
     {
         welcomePopup.SetActive(false);
+        POIs.SetActive(true);
 
     }
 }
