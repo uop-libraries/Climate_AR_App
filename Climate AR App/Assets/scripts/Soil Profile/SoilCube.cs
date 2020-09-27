a060901e8028192d56a2b2b73e03c5141fe49c1e
the_fashionable_coder <k_canniff@u.pacific.edu>
2020-09-26 22:34:30 -0700

    basic soil erosion done

--- Climate AR App/Assets/scripts/Soil Profile/SoilCube.cs
+++ Climate AR App/Assets/scripts/Soil Profile/SoilCube.cs
@@ -12,20 +12,37 @@ using UnityEngine;
  */
 public class SoilCube : MonoBehaviour
 {
-    public bool isTopCube;
+    public bool greenTopCube;
+    public GameObject brownCube;
     public float erosionAmount; //the amount that the top will be shrunk by 
     public Material goodSoilColor;
     public Material badSoilColor;
+    public GameObject grass;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         StartErosion();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (greenTopCube && anim.GetBool("isErosionTime") && gameObject.GetComponent<Renderer>().enabled && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1) //erosion animation is done playing. If normalizedTime is 0 to 1 = playing, if greater than 1 = finished
+        {
+            this.GetComponent<Renderer>().enabled = false; //hide the green
+            if (grass != null)
+            {
+                grass.SetActive(false);
+            }
+            if (brownCube != null)
+            {
+                brownCube.GetComponent<Animator>().SetBool("isErosionBrownTime", true);// start the erosion animation
+                brownCube.GetComponent<Animator>().SetFloat("erosionSpeed", erosionAmount); // adjust the speed of animation. this ammount is a multiplied amount
+
+            }
+        }
     }
 
     /**
@@ -53,8 +70,10 @@ public class SoilCube : MonoBehaviour
     {
         //get animation paramter for speed
         //animations[i].GetComponent<Animator>().SetBool("StartRiverIncrease", true); //start the animation, the string value is a parameter from the animator window
-        GetComponent<Animator>().SetBool("isErosionTime", true);// start the erosion animation
-        GetComponent<Animator>().SetFloat("erosionSpeed", erosionAmount); // adjust the speed of animation. this ammount is a multiplied amount
+        
+        anim.SetBool("isErosionTime", true);// start the erosion animation
+        
+        anim.SetFloat("erosionSpeed", erosionAmount); // adjust the speed of animation. this ammount is a multiplied amount
 
 
     }
@@ -63,4 +82,6 @@ public class SoilCube : MonoBehaviour
     {
         Eroode();
     }
+
+
 }
